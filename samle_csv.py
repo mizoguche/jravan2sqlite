88f8a73e38ff7ed880fb9503fba7fb1b4a45e8e3
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2017-06-19 20:25:24 +0900

    Read csv with pandas

---
+++ samle_csv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+names = ('Year', 'Month', 'Day', 'Course', 'RaceNumber', 'ClassCode', 'GradeCode', 'TurfOrDirt', 'TrackCode',
+         'TrackCodeJV', 'CornerNumber', 'Distance', 'CourseDivision', 'State', 'Weather', 'TotalHorseNumber',
+         'AgeLimitationCode', 'RaceSymbolCode', 'WeightCode', 'RaceIDOld', 'RaceID', 'HorseNumber', 'PostPosition',
+         'Name', 'Gender', 'Age', 'Jockey', 'Weight', 'Blinker', 'OfficialOrderOfFinish', 'OrderOfFinish', 'ErrorCode',
+         'Margin', 'OrderOfFavorite', 'OddsOfWin', 'TimeOfSeconds', 'OrderOf1stCorner', 'OrderOf2ndCorner',
+         'OrderOf3rdCorner', 'OrderOf4thCorner', 'RunningStyle', 'Last3FurlongTime', 'Last3FurlongOrder',
+         'Average3FurlongTime', 'HorseWeight', 'HorseWeightDelta', 'Trainer', 'BloodRegistrationID', 'JockeyCode',
+         'TrainerCode', 'Producer', 'Sire', 'Broodmare', 'BroodmareSire', 'BirthDate')
+
+df = pd.read_csv('~/Desktop/results_test.csv', names=names)
+
+print(df)
